9fc412a7e9f98fdc92d08c0294b62699caa65ba0
willyklindt <wgk@ruc.dk>
2022-11-01 15:51:12 +0100

    Screenshot og controls

--- PCG/Assets/_Scripts/PlayerControl.cs
+++ PCG/Assets/_Scripts/PlayerControl.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
+using Input = UnityEngine.Input;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    [SerializeField]
+    public float speed = 10f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement; //stores X & Y-value
 
     // Update is called once per frame
-    void Update()
+    void Update() //input
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 10);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate() //executed on a fixed time -- movement
+    {
+        //rb = variable for player. currentposition + new movment x & y * speed
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
