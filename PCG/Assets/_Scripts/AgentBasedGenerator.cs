43a75dca1ec00fef7fbddf9699e5d97ee8592945
willyklindt <wgk@ruc.dk>
2022-11-29 13:05:28 +0100

    GREAT!

---
+++ PCG/Assets/_Scripts/AgentBasedGenerator.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Random = UnityEngine.Random;
+
+public class AgentBasedGenerator : MonoBehaviour
+{
+    [SerializeField]
+    protected Vector2Int startPos = new Vector2Int(0, 0);
+
+    [SerializeField]
+    private int iterations = 30; //CHECK DEN HER
+
+    [SerializeField]
+    public int walkDist = 10;
+
+    [SerializeField]
+    public bool randomWalk = true;
+
+    public void MapGenerator()
+    {
+        HashSet<Vector2Int> groundPos = RandomPath();
+        foreach (var pos in groundPos)
+        {
+            Debug.Log(pos);
+        }
+    }
+
+    protected HashSet<Vector2Int> RandomPath()
+    {
+        var currPos = startPos;
+        HashSet<Vector2Int> groundPos = new HashSet<Vector2Int>();
+        for (int i = 0; i < iterations; i++)
+        {
+            var path = DungeonGenerator.AgentBasedWalk(currPos, walkDist);
+            groundPos.UnionWith(path);
+            if (randomWalk)
+            {
+                currPos = groundPos.ElementAt(Random.Range(0, groundPos.Count));
+            }
+        }
+        return groundPos;
+    }
+}
---
+++ PCG/Assets/_Scripts/DungeonGenerator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DungeonGenerator
+{
+    public static HashSet<Vector2Int> AgentBasedWalk (Vector2Int startPos, int walkDist)
+    {
+        HashSet<Vector2Int> path = new HashSet<Vector2Int>();
+
+        path.Add(startPos);
+        var prevPos = startPos;
+
+        for (int i = 0; i < walkDist; i++)
+        {
+            var nextPos = prevPos + Direction.GetPath();
+            prevPos = nextPos;
+            path.Add(nextPos);
+        }
+        return path;
+    }
+}
+
+public static class Direction
+{
+    public static List<Vector2Int> directionList = new List<Vector2Int>
+    {
+        new Vector2Int (0, 1), // Direction UP
+        new Vector2Int (1, 0), // Direction RIGHT
+        new Vector2Int (0, -1), // Direction DOWN
+        new Vector2Int (-1, 0) // Direction LEFT
+    };
+
+    public static Vector2Int GetPath()
+    {
+        return directionList[Random.Range(0, directionList.Count)];
+    }
+}
