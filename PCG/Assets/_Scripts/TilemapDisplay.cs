bf91957da8f0b1632984095053cc1a1e9721be5f
TechDiesel <112946584+TechDiesel@users.noreply.github.com>
2022-12-06 14:50:36 +0100

    Update of wall placement

--- PCG/Assets/_Scripts/TilemapDisplay.cs
+++ PCG/Assets/_Scripts/TilemapDisplay.cs
@@ -7,10 +7,52 @@ using UnityEngine.Tilemaps;
 public class TilemapDisplay : MonoBehaviour
 {
     [SerializeField]
-    private Tilemap groundTilemap, wallTilemap;
+    private Tilemap groundTilemap;
 
     [SerializeField]
-    private TileBase groundTiles, wallTop;
+    private Tilemap wallTilemap;
+
+    [SerializeField]
+    private TileBase groundTiles;
+
+    [SerializeField]
+    private TileBase wallTop;
+
+
+    [SerializeField]
+    private TileBase wallRight;
+
+
+    [SerializeField]
+    private TileBase wallLeft;
+
+
+    [SerializeField]
+    private TileBase wallBottom;
+
+
+    [SerializeField]
+    private TileBase wallMid;
+
+    [SerializeField]
+    private TileBase wallInnerCornerDownLeft;
+
+    [SerializeField]
+    private TileBase wallInnerCornerDownRight;
+
+
+    [SerializeField]
+    private TileBase wallDiagonalCornerDownRight;
+
+
+    [SerializeField]
+    private TileBase wallDiagonalCornerDownLeft;
+
+    [SerializeField]
+    private TileBase wallDiagonalCornerUpRight;
+
+    [SerializeField]
+    private TileBase wallDiagonalCornerUpLeft;
 
     public void createGroundTiles(IEnumerable<Vector2Int> groundPos)
     {
@@ -27,8 +69,36 @@ public class TilemapDisplay : MonoBehaviour
 
     internal void createSingleWall(Vector2Int pos, string Binary)
     {
-        Debug.Log(pos´" type:")
-        createSingleTile(wallTilemap, wallTop, pos);
+        int IntType = Convert.ToInt32(Binary, 2);
+        TileBase tile = null;
+        if (WallBytes.wallTop.Contains(IntType))
+        {
+            tile = wallTop;
+        }
+        else if(WallBytes.wallSideRight.Contains(IntType))
+        {
+            tile = wallRight;
+        }
+        else if (WallBytes.wallSideLeft.Contains(IntType))
+        {
+            tile = wallLeft;
+        }
+
+        else if (WallBytes.wallBottm.Contains(IntType))
+        {
+            tile = wallBottom;
+        }
+
+        else if (WallBytes.wallFull.Contains(IntType))
+        {
+            tile = wallMid;
+        }
+        if (tile!=null)
+        {
+            
+    
+        createSingleTile(wallTilemap, tile, pos);
+        }
     }
 
     private void createSingleTile(Tilemap tilemap, TileBase tiles, Vector2Int pos)
@@ -43,8 +113,55 @@ public class TilemapDisplay : MonoBehaviour
         wallTilemap.ClearAllTiles();
     }
 
-    internal void createSingleCornerWall(Vector2Int pos, string binaryWalls)
+    internal void createSingleCornerWall(Vector2Int pos, string Binary)
     {
-        throw new NotImplementedException();
+        int IntType = Convert.ToInt32(Binary, 2);
+        TileBase tile = null;
+
+        if(WallBytes.wallInnerCornerDownLeft.Contains(IntType))
+        {
+            tile = wallInnerCornerDownLeft;
+        }
+        else if (WallBytes.wallInnerCornerDownRight.Contains(IntType))
+        {
+            tile = wallInnerCornerDownRight;
+        }
+
+        else if (WallBytes.wallDiagonalCornerDownLeft.Contains(IntType))
+        {
+            tile = wallDiagonalCornerDownLeft;
+        }
+
+        else if (WallBytes.wallDiagonalCornerDownRight.Contains(IntType))
+        {
+            tile = wallDiagonalCornerDownRight;
+        }
+
+        else if (WallBytes.wallDiagonalCornerUpRight.Contains(IntType))
+        {
+            tile = wallDiagonalCornerUpRight;
+        }
+
+        else if (WallBytes.wallDiagonalCornerUpLeft.Contains(IntType))
+        {
+            tile = wallDiagonalCornerUpLeft;
+        }
+
+        else if (WallBytes.wallFullEightDirections.Contains(IntType))
+        {
+            tile = wallBottom;
+        }
+
+        else if (WallBytes.wallBottmEightDirections.Contains(IntType))
+        {
+            tile = wallMid;
+        }
+
+
+        if (tile!= null)
+        {
+
+            createSingleTile(wallTilemap, tile, pos);
+        }
     }
 }
